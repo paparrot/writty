58aba384fd103a72741e6516a40bd79ffbbc6c0b
Klim <subbotin.klim@gmail.com>
2023-08-13 10:14:06 +0200

    update js build

---
+++ public/build/assets/Create-1fc96def.js
@@ -0,0 +1,1 @@
+import{_ as a,a as e}from"./DefaultLayout-91e70b0e.js";import{o as t,p as _,w as r,a as s}from"./app-f1fa24f4.js";const f={__name:"Create",setup(o){return(c,n)=>(t(),_(a,null,{default:r(()=>[s(e)]),_:1}))}};export{f as default};
--- public/build/assets/Create-962c3d10.js
+++
@@ -1,1 +0,0 @@
-import{_ as a,a as e}from"./DefaultLayout-ff5454a1.js";import{o as t,p as _,w as r,a as s}from"./app-beabed82.js";const f={__name:"Create",setup(o){return(c,n)=>(t(),_(a,null,{default:r(()=>[s(e)]),_:1}))}};export{f as default};
