778352d0ed74d5c6a2b2156eb8022690d03aad79
Klim <subbotin.klim@gmail.com>
2023-08-15 22:09:57 +0200

    add id field to user resource

--- app/Http/Resources/Post/UserResource.php
+++ app/Http/Resources/Post/UserResource.php
@@ -17,6 +17,7 @@ class UserResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id,
             'name' => $this->name,
             'avatar' => $this->profile_photo_path,
             'nickname' => $this->nickname
