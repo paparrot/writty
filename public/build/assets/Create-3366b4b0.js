49368b607626ab64c161692b3acc6e573139b298
Klim <subbotin.klim@gmail.com>
2023-08-16 16:57:45 +0200

    test update js build

--- public/build/assets/Create-3366b4b0.js
+++
@@ -1,1 +0,0 @@
-import{_ as a,a as e}from"./DefaultLayout-c395d98b.js";import{o as t,p as _,w as r,a as s}from"./app-cd6960fd.js";const f={__name:"Create",setup(o){return(c,n)=>(t(),_(a,null,{default:r(()=>[s(e)]),_:1}))}};export{f as default};
