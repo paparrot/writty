186efe01161ff6ea78e5f6763caf5a18fb7ca920
Klim <subbotin.klim@gmail.com>
2023-08-19 23:51:58 +0200

    update static

---
+++ public/build/assets/Create-4e4d007b.js
@@ -0,0 +1,1 @@
+import{_ as a,P as e}from"./DefaultLayout-a4201d37.js";import{o as t,p as o,w as r,a as s}from"./app-13a24cae.js";const f={__name:"Create",setup(_){return(c,m)=>(t(),o(a,null,{default:r(()=>[s(e)]),_:1}))}};export{f as default};
