0dcd432d6e6fd89dc4bb25c00d89e9ba3b67507b
Klim <subbotin.klim@gmail.com>
2023-08-19 12:08:16 +0200

    update tests

--- tests/Feature/RegistrationTest.php
+++ tests/Feature/RegistrationTest.php
@@ -49,6 +49,7 @@ class RegistrationTest extends TestCase
         $response = $this->post('/register', [
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'nickname' => 'testuser',
             'password' => 'password',
             'password_confirmation' => 'password',
             'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature(),
